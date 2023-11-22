371abf4e6303dd05ab527f06c6499519c89990c7
JOSE <jose@Julianas-MacBook-Air.local>
2023-11-21 22:00:15 -0500

    trabajo corregido

--- js/script.js
+++ js/script.js
@@ -1,40 +1,95 @@
 
+// const listaUsuarios = document.getElementById('listaUsuarios');
 
-const listaUsuarios = document.getElementById('listaUsuarios');
+// console.log(listaUsuarios);
 
-console.log(listaUsuarios);
+// let edadAleatoria = Math.floor(Math.random()*48)+18;
+// console.log(edadAleatoria);
 
-let edadAleatoria = Math.floor(Math.random()*48)+18;
-console.log(edadAleatoria);
-
-fetch('https://jsonplaceholder.typicode.com/users/')
-    .then ((response) => {
-        if (!response.ok) {
-            throw new Error ('La solicitud no se ha podido procesar');
-        }
-        return response.json();
-    })
-    .then ((data) => {
-        // console.log(data);
-        const newArrayUsers = data.map(user => {
-            return {
-                ...user,
-                edad: edadAleatoria, //FIXME:
-            }
+// fetch('https://jsonplaceholder.typicode.com/users/')
+//     .then ((response) => {
+//         if (!response.ok) {
+//             throw new Error ('La solicitud no se ha podido procesar');
+//         }
+//         return response.json();
+//     })
+//     .then ((data) => {
+//         // console.log(data);
+//         const newArrayUsers = data.map(user => {
+//             return {
+//                 ...user,
+//                 edad: edadAleatoria, //FIXME:
+//             }
             
-        })
-        console.log(newArrayUsers);
+//         })
+//         console.log(newArrayUsers);
 
-        listaUsuarios.innerHTML = `
+//         listaUsuarios.innerHTML = `
+//         <li>
+//             <div>
+//                 <p>${newArrayUsers}
+//                 </p>
+//             </div>
+//         </li>
+//         `
+
+//         // const {name, edadAleatoria, username, phone, email, company, address} = data[0];
+//         // console.log(name, edadAleatoria, username, phone, email, company.name, address.street, address.suite, address.city);
+//         // let usuario = [];
+//     });
+
+//  CORREGIDO EN CLASE:
+
+
+const listUsers = document.getElementById('listaUsuarios');
+console.log(listUsers);
+
+fetch("https://jsonplaceholder.typicode.com/users") 
+.then(res => {
+    if(!res.ok) {
+        throw new Error('Error cargando los usuarios')
+    }
+    return res.json()
+}) 
+.then (data => {
+    console.log(data)
+    const usersModified = [];
+    data.forEach(user => {
+        console.log(user)
+        const newUser = {
+            ...user,
+            age: randomAge(),
+            // ./assets/img/1.jpeg
+            img: `./assets/img/${user.id}.jpeg`
+        }
+        usersModified.push(newUser)
+        console.log(usersModified);
+        
+    })
+    showUsers(usersModified);
+})
+
+function randomAge() {
+    return Math.floor(Math.random()*48)+18;
+}
+
+function showUsers(users) {
+    users.forEach(user => {
+        console.log(user);
+        const {name, age, username, img, phone, email, company, address} = user;
+        const {name:companyName} = company;
+        const {street, suite, city} = address;
+        listUsers.innerHTML += `
         <li>
-            <div>
-                <p>${newArrayUsers}
-                </p>
-            </div>
-        </li>
-        `
+            <p><strong>Nombre: </strong> ${name}</p>
+            <p><strong>Edad: </strong> ${age}</p>
+            <p><strong>Username: </strong> ${username}</p>
+            <p><strong> Telefono: </strong> ${phone}</p>
+            <p><strong> Email: </strong> ${email}</p>
+            <p><strong> Compañía: </strong> ${companyName}</p>
+            <p><strong> Dirección: </strong> ${street}, ${suite}, ${city}</p>
+            <img src="${img}" alt="${name}">
+        </li>`
 
-        // const {name, edadAleatoria, username, phone, email, company, address} = data[0];
-        // console.log(name, edadAleatoria, username, phone, email, company.name, address.street, address.suite, address.city);
-        // let usuario = [];
     });
+}
